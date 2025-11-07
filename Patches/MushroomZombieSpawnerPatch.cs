621eb43e878099cd0e314ddc40a6309f950da0cf
Huskslay <huskylover269@outlook.com>
2025-11-07 13:15:17 +0000

    Initial commit

---
+++ Patches/MushroomZombieSpawnerPatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using HarmonyLib;
+
+namespace NoZombies.Patches;
+
+[HarmonyPatch(typeof(MushroomZombieSpawner))]
+public class MushroomZombieSpawnerPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(MushroomZombieSpawner.Start))]
+    public static bool StartPrefix(MushroomZombieSpawner __instance)
+    {
+        UnityEngine.Object.Destroy(__instance.gameObject);
+        Plugin.Logger.LogMessage($"No zombies, removing zombie spawner!");
+        return false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using System;
+using UnityEngine;
+using BepInEx.Configuration;
+
+
+namespace NoZombies;
+
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+
+    public static readonly KeyCode GUIKey = KeyCode.Home;
+
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        Patch();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Patch()
+    {
+        Harmony patcher = new("HarmonyPatcher");
+        patcher.PatchAll();
+    }
+}
